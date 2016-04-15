1d0efbf6279d305cfebb15dcbb35ae5ed150c3bb
iggdrasia <tearmat123@gmail.com>
2016-04-15 10:49:19 +0700

    #01 Update01
    Have blnk windows.

---
+++ Main.java
@@ -0,0 +1,15 @@
+//package f2.5610110338;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class Main{
+	public static void main(String[] args){
+		JFrame frame = new JFrame("Space War");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 650);
+		frame.getContentPane().setLayout(new BorderLayout());
+		frame.setVisible(true);
+	}
+}
